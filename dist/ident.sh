4cf12a835538ec78df44f4f99b941197f48a041d
Michael Teichgräber <mteichgraeber@gmx.de>
2021-06-20 23:15:17 +0200

    dist/*.sh: no quotes needed around single word cmds

--- dist/ident.sh
+++ dist/ident.sh
@@ -1,8 +1,8 @@
 ./bin/openocd-efm32s2 -s scripts -f interface/cmsis-dap.cfg \
 	-f target/efm32s2.cfg\
 	-c 'transport select swd' \
-	-c 'init' \
-	-c 'halt' \
+	-c init \
+	-c halt \
 	-c 'flash probe 0' \
 	-c 'flash banks' \
 	-c 'flash list' \
