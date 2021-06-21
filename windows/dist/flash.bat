5086f731204cd344a11ddf6ae2fc658e383b636f
Michael Teichgräber <mteichgraeber@gmx.de>
2021-06-21 10:58:35 +0200

    windows/dist/flash.bat: escape backslashes in hex file path

--- windows/dist/flash.bat
+++ windows/dist/flash.bat
@@ -1,3 +1,5 @@
+set hexfile=%1
+set hexfileesc=%hexfile:\=\\%
 .\bin\openocd-efm32s2 -s scripts -f interface\cmsis-dap.cfg ^
 	-f target\efm32s2.cfg ^
 	-c "transport select swd" ^
@@ -6,7 +8,7 @@
 	-c "flash probe 0" ^
 	-c "flash banks" ^
 	-c "flash list" ^
-	-c "flash write_image erase %1" ^
+	-c "flash write_image erase %hexfileesc%" ^
 	-c exit
 
 pause
